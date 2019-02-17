dd1d870cca01797a20ef848f7e1f3d623a9cd46b
wangerkang <wangerk@163.com>
2019-02-18 05:09:24 +0800

    update

--- yuanxiaojie/js/index.js
+++ yuanxiaojie/js/index.js
@@ -71,7 +71,7 @@ function start() {
 		3, 2, 4,2,5, 2, 6, 2, 7, 2, 8, 2,9,2,10,2,
 		5, 3, 6, 3, 7, 3, 8, 3
 	];
-	var textString1 = 'il0vey0uforever';
+	var textString1 = 'il0vey0uf0rever';
 	var textMatrix1 = [
 		6.5,0,
 		5, 1, 6, 1, 7, 1, 8,1,
